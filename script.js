26450f1427be2ccf4a31281071663aa1ee72a9e1
Pedro Henrique <139656791+PedroAlvesDev@users.noreply.github.com>
2024-05-28 19:43:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    const body = document.getElementById('root');
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        body.classList.add('dark');
+        return;
+    }
+
+    mode.classList.add('fa-moon');
+    mode.classList.remove('fa-sun');
+    body.classList.remove('dark');
+});
