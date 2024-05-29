674c0f169dc06a2d7c6ce6ad688f8d9a80c3d390
Pedro Henrique <139656791+PedroAlvesDev@users.noreply.github.com>
2024-05-29 10:37:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+
 const mode = document.getElementById('mode_icon');
+const body = document.getElementById('root');
+
+function applyDarkMode() {
+    mode.classList.remove('fa-moon');
+    mode.classList.add('fa-sun');
+    body.classList.add('dark');
+}
+
+function applyLightMode() {
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon')
+    body.classList.remove('dark');
+}
+
+if(localStorage.getItem('theme') === 'dark') {
+    applyDarkMode();;
+} else {
+    applyLightMode()
+}
 
 mode.addEventListener('click', () => {
-    const body = document.getElementById('root');
     if(mode.classList.contains('fa-moon')) {
-        mode.classList.remove('fa-moon');
-        mode.classList.add('fa-sun');
-
-        body.classList.add('dark');
-        return;
+        applyDarkMode();
+        localStorage.setItem('theme', 'dark');
+    } else {
+        applyLightMode();
+        localStorage.setItem('theme', 'light');
     }
-
-    mode.classList.add('fa-moon');
-    mode.classList.remove('fa-sun');
-    body.classList.remove('dark');
-});
+}) ;
